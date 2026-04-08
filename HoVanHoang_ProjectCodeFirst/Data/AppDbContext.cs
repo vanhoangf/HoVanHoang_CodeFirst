b2eaf2da7700cc4939a7fe7a6f12418cc3d89f73
Hồ Hoàng <hovanhoang959@gmail.com>
2026-04-08 12:04:15 +0700

    Project sau khi cau hinh DbContext, chuoi ket noi va dependencyInjection

---
+++ HoVanHoang_ProjectCodeFirst/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HoVanHoang_ProjectCodeFirst.Models;
+
+namespace HoVanHoang_ProjectCodeFirst.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+        public DbSet<LopHoc> LopHocs { get; set; }
+        public DbSet<SinhVien> SinhViens { get; set; }
+    }
+}
