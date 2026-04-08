15f18f045cffe23a87ea648f88bd9d43030de3f9
Hồ Hoàng <hovanhoang959@gmail.com>
2026-04-08 11:29:19 +0700

    Tao cac Class trong thu muc Models

---
+++ HoVanHoang_ProjectCodeFirst/Models/LopHoc.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+namespace HoVanHoang_ProjectCodeFirst.Models
+{
+    public class LopHoc
+    {
+        public int LopHocId { get; set; }
+        public string TenLop { get; set; }
+        public string Khoa { get; set; }
+
+        public List<SinhVien> SinhViens { get; set; }
+    }
+}
---
+++ HoVanHoang_ProjectCodeFirst/Models/SinhVien.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HoVanHoang_ProjectCodeFirst.Models
+{
+    public class SinhVien
+    {
+        public int SinhVienId { get; set; }
+        public string HoTen { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public double DiemTrungBinh { get; set; }
+
+        public int LopHocId { get; set; }
+        public LopHoc LopHoc { get; set; }
+    }
+}
