b983967509ef73d7588b78899a65c28024f4390f
Hồ Hoàng <hovanhoang959@gmail.com>
2026-04-08 11:15:37 +0700

    Tao Project va cai dat thu vien

---
+++ HoVanHoang_ProjectCodeFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HoVanHoang_ProjectCodeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
